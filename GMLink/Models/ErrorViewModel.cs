7ce28ed0574be7965c6ebc5bce42e51233a9833e
johnp <johnp@DESKTOP-LNRJ72C>
2023-04-18 20:34:57 -0400

    Add project files.

---
+++ GMLink/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GMLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
