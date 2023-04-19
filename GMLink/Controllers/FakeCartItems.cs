fbf11048eec32ada5fb95a1869d5d321d201788d
El-Duende <Johnpete.foster@gmail.com>
2023-04-19 13:11:44 -0400

    John

---
+++ GMLink/Controllers/FakeCartItems.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace GMlink.Models
+{
+    public class FakeCartItems
+    {
+
+        public int ReservationID { get; set; }
+ 
+        public int GameMasterID { get; set; }
+
+
+        public string GroupID { get; set; }
+
+        public string DateTime { get; set; }
+    }
+}
