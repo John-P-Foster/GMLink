1df1d14064bd5c59515a2b80cb12b93e810f0669
El-Duende <Johnpete.foster@gmail.com>
2023-04-21 18:32:59 -0400

    Added check out for reservations

---
+++ GMLink/Controllers/PurchaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GMLink.Models;
+
+namespace GMLink.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public ViewResult Checkout() => View (new Purchase());
+    }
+}
---
+++ GMLink/Models/Purchase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GMLink.Models
+{
+    public class Purchase
+    {
+        [BindNever]
+        public int PurchaseID { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; } = new List<CartLine>();
+        public string GroupName { get; set; }
+ 
+    } 
+}
