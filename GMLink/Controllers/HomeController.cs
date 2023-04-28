9e5cab3f06ce26407fa601a01ebc664fa663b88e
Matthieu Joseph <allenmatt37@gmail.com>
2023-04-28 17:14:42 -0400

    Controller

--- GMLink/Controllers/HomeController.cs
+++ GMLink/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace GMLink.Controllers
         {
             return View();
         }
+        public IActionResult Policies()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
