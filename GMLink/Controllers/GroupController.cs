10e6b1ad1233d00d71ddbe49f0465514d0834b7d
rileytittle <coletittle@ymail.com>
2023-04-28 15:03:52 -0400

    Started groups

---
+++ GMLink/Controllers/GroupController.cs
@@ -0,0 +1,6 @@
+﻿namespace GMLink.Controllers
+{
+    public class GroupController
+    {
+    }
+}
---
+++ GMLink/Models/IGroupRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace GMLink.Models
+{
+    public interface IGroupRepository
+    {
+        IEnumerable<Group> Groups { get; }
+        void SaveGroup(Group group);
+    }
+}
